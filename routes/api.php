5f28afe026d7319f200c6f0ea6862f45cad563b6
chad83 <wehbe.chady@gmail.com>
2022-11-11 14:46:15 +0100

    Added basic api routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-    // TEST
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('v1')->group(function () {
+
+    // Sessions
+    Route::prefix('session')->group(function () {
+    });
+
 });

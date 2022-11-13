2dd70f01df3fb6ee1b109f2b19d464cfa9a9a157
chad83 <wehbe.chady@gmail.com>
2022-11-13 13:33:47 +0100

    RET-4
    - Posts can now be saved via POST request
    - Added participant getter by key

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    private ParticipantController $participantController;
+
+    public function __construct(ParticipantController $participantController, SessionController $sessionController)
+    {
+        $this->participantController = $participantController;
+    }
+
+    public function create(Request $request)
+    {
+        // Get the participant.
+        $participant = $this->participantController->find($request->participantKey);
+
+        $post = Post::create([
+            'participant_id' => $participant->id,
+            'session_id' => $participant->session_id,
+            'category' => $request->category,
+            'text' => $request->text
+        ]);
+        $post->save();
+
+        return $post->fresh();
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,8 @@ class Post extends Model
         'likes' => 0
     ];
 
+    protected $fillable = ['session_id', 'participant_id', 'category', 'text'];
+
     protected $hidden = ['id', 'session_id', 'participant_id', 'created_at', 'updated_at'];
 
     public function participant(): BelongsTo
